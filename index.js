44a1414c99990236123ad4b27c385ff484bfccc6
Szymon <70262692+mikro8@users.noreply.github.com>
2021-04-15 12:46:51 +0200

    added type validation

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function googleHomeSpeak() {
 }
 
 googleHomeSpeak.prototype.init = function (ip) {
+  if (typeof ip != "string")
+    throw "ip argument must be string example: 192.168.1.200";
   return new Promise((resolve, reject) => {
     this._client.connect(ip, () => {
       this._client.launch(DefaultMediaReceiver, (err, player) => {
@@ -21,6 +23,10 @@ googleHomeSpeak.prototype.init = function (ip) {
 };
 
 googleHomeSpeak.prototype.speak = function (text, lang) {
+  if (typeof text != "string")
+    throw "text argument must be string example: 'Hello World!'";
+  if (typeof lang != "string")
+    throw "lang argument must be undefined string example: 'de'";
   const url = googleTTS.getAudioUrl(text, {
     lang: lang,
     slow: false,
